eec35c7e76366b6f8967a0457b471b30da074efe
Khanh Duong <dqkqdlot@gmail.com>
2026-09-05 21:41:45 +0900

    test: docs

--- doctest/Main.hs
+++ doctest/Main.hs
@@ -1,4 +1,9 @@
 import Test.DocTest
 
 main :: IO ()
-main = doctest ["src/Homework1.hs"]
+main =
+  doctest
+    [ "src/Homework1.hs"
+    , "src/Homework2/LogAnalysis.hs"
+    , "src/Homework2/Log.hs"
+    ]
