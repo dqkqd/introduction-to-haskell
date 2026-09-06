17280d10eef96e82eaba72d888c7ded8751c2137
Khanh Duong <dqkqdlot@gmail.com>
2026-09-06 18:26:10 +0900

    chore: typo

--- test/TestHomework6.hs
+++ test/TestHomework6.hs
@@ -10,6 +10,7 @@ spec = do
       it "fibs1" $
         take 10 fibs1 `shouldBe` [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
 
-    describe "Exercise 1" $ do
+    describe "Exercise 2" $ do
       it "fibs2" $
-        take 15 fibs2 `shouldBe` [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377]
+        take 15 fibs2
+          `shouldBe` [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377]
