9f212c1c684d5fd5b34e0175987352028b2f0cb0
Khanh Duong <dqkqdlot@gmail.com>
2026-09-06 15:56:27 +0900

    feat: ex2

--- src/Homework5/Calc.hs
+++ src/Homework5/Calc.hs
@@ -1,9 +1,15 @@
-module Homework5.Calc (eval) where
+module Homework5.Calc (eval, evalStr) where
 
 import Homework5.ExprT (ExprT (Add, Lit, Mul))
+import Homework5.Parser (parseExp)
 
 eval :: ExprT -> Integer
 eval x = case x of
   Lit v -> v
   Add l r -> eval l + eval r
   Mul l r -> eval l * eval r
+
+evalStr :: String -> Maybe Integer
+evalStr program = do
+  prog <- parseExp Lit Add Mul program
+  return (eval prog)
--- test/TestHomework5.hs
+++ test/TestHomework5.hs
@@ -1,6 +1,6 @@
 module TestHomework5 (spec) where
 
-import Homework5.Calc (eval)
+import Homework5.Calc (eval, evalStr)
 import Homework5.ExprT
 import Test.Hspec
 
@@ -10,3 +10,7 @@ spec = do
     describe "Exercise 1" $ do
       it "eval" $
         eval (Mul (Add (Lit 2) (Lit 3)) (Lit 4)) `shouldBe` 20
+
+    describe "Exercise 2" $ do
+      it "evalStr" $
+        evalStr "(2+3)*4" `shouldBe` Just 20
