18c609c5d0fc3a2602c9d4e69e06a29df0f5c893
Khanh Duong <dqkqdlot@gmail.com>
2026-09-06 11:21:45 +0900

    chore: warning

--- test/TestHomework3.hs
+++ test/TestHomework3.hs
@@ -13,7 +13,7 @@ spec = do
       skips "hello!" `shouldBe` ["hello!", "el!", "l!", "l", "o", "!"]
 
     it "skips [1]" $
-      skips [1] `shouldBe` [[1]]
+      skips [1 :: Int] `shouldBe` [[1]]
 
     it "skips [True,False]" $
       skips [True, False] `shouldBe` [[True, False], [False]]
