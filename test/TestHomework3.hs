699a8a2d52eba27220d15543865cd486cf038682
Khanh Duong <dqkqdlot@gmail.com>
2026-09-06 12:49:44 +0900

    chore: describe homework3

--- test/TestHomework3.hs
+++ test/TestHomework3.hs
@@ -5,50 +5,51 @@ import Test.Hspec
 
 spec :: Spec
 spec = do
-  describe "Exercise 1: skips" $ do
-    it "skips \"ABCD\"" $
-      skips "ABCD" `shouldBe` ["ABCD", "BD", "C", "D"]
+  describe "Homework3" $ do
+    describe "Exercise 1: skips" $ do
+      it "skips \"ABCD\"" $
+        skips "ABCD" `shouldBe` ["ABCD", "BD", "C", "D"]
 
-    it "skips \"hello!\"" $
-      skips "hello!" `shouldBe` ["hello!", "el!", "l!", "l", "o", "!"]
+      it "skips \"hello!\"" $
+        skips "hello!" `shouldBe` ["hello!", "el!", "l!", "l", "o", "!"]
 
-    it "skips [1]" $
-      skips [1 :: Int] `shouldBe` [[1]]
+      it "skips [1]" $
+        skips [1 :: Int] `shouldBe` [[1]]
 
-    it "skips [True,False]" $
-      skips [True, False] `shouldBe` [[True, False], [False]]
+      it "skips [True,False]" $
+        skips [True, False] `shouldBe` [[True, False], [False]]
 
-    it "skips []" $
-      skips ([] :: [Int]) `shouldBe` ([] :: [[Int]])
+      it "skips []" $
+        skips ([] :: [Int]) `shouldBe` ([] :: [[Int]])
 
-  describe "Exercise 2: localMaxima" $ do
-    it "localMaxima [2,9,5,6,1]" $
-      localMaxima [2, 9, 5, 6, 1] `shouldBe` [9, 6]
+    describe "Exercise 2: localMaxima" $ do
+      it "localMaxima [2,9,5,6,1]" $
+        localMaxima [2, 9, 5, 6, 1] `shouldBe` [9, 6]
 
-    it "localMaxima [2,3,4,1,5]" $
-      localMaxima [2, 3, 4, 1, 5] `shouldBe` [4]
+      it "localMaxima [2,3,4,1,5]" $
+        localMaxima [2, 3, 4, 1, 5] `shouldBe` [4]
 
-    it "localMaxima [1,2,3,4,5]" $
-      localMaxima [1, 2, 3, 4, 5] `shouldBe` []
+      it "localMaxima [1,2,3,4,5]" $
+        localMaxima [1, 2, 3, 4, 5] `shouldBe` []
 
-  describe "Exercise 3: histogram" $ do
-    it "histogram [1,1,1,5]" $
-      histogram [1, 1, 1, 5]
-        `shouldBe` unlines
-          [ " *        "
-          , " *        "
-          , " *   *    "
-          , "=========="
-          , "0123456789"
-          ]
+    describe "Exercise 3: histogram" $ do
+      it "histogram [1,1,1,5]" $
+        histogram [1, 1, 1, 5]
+          `shouldBe` unlines
+            [ " *        "
+            , " *        "
+            , " *   *    "
+            , "=========="
+            , "0123456789"
+            ]
 
-    it "histogram [1,4,5,4,6,6,3,4,2,4,9]" $
-      histogram [1, 4, 5, 4, 6, 6, 3, 4, 2, 4, 9]
-        `shouldBe` unlines
-          [ "    *     "
-          , "    *     "
-          , "    * *   "
-          , " ******  *"
-          , "=========="
-          , "0123456789"
-          ]
+      it "histogram [1,4,5,4,6,6,3,4,2,4,9]" $
+        histogram [1, 4, 5, 4, 6, 6, 3, 4, 2, 4, 9]
+          `shouldBe` unlines
+            [ "    *     "
+            , "    *     "
+            , "    * *   "
+            , " ******  *"
+            , "=========="
+            , "0123456789"
+            ]
