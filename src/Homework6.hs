e5014ba882a7396a8137168f1372e8da0199adc8
Khanh Duong <dqkqdlot@gmail.com>
2026-09-06 18:25:15 +0900

    feat: ex2

--- src/Homework6.hs
+++ src/Homework6.hs
@@ -1,4 +1,4 @@
-module Homework6 (fibs1) where
+module Homework6 (fibs1, fibs2) where
 
 fib :: Integer -> Integer
 fib 0 = 0
@@ -7,3 +7,6 @@ fib n = fib (n - 1) + fib (n - 2)
 
 fibs1 :: [Integer]
 fibs1 = map fib [0 ..]
+
+fibs2 :: [Integer]
+fibs2 = 0 : 1 : zipWith (+) fibs2 (drop 1 fibs2)
--- test/TestHomework6.hs
+++ test/TestHomework6.hs
@@ -1,6 +1,6 @@
 module TestHomework6 (spec) where
 
-import Homework6 (fibs1)
+import Homework6 (fibs1, fibs2)
 import Test.Hspec
 
 spec :: Spec
@@ -9,3 +9,7 @@ spec = do
     describe "Exercise 1" $ do
       it "fibs1" $
         take 10 fibs1 `shouldBe` [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
+
+    describe "Exercise 1" $ do
+      it "fibs2" $
+        take 15 fibs2 `shouldBe` [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377]
