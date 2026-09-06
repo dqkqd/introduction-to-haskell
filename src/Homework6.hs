3ecaa4b598ea606129e583bebe7b1b8f2881c32b
Khanh Duong <dqkqdlot@gmail.com>
2026-09-07 07:57:40 +0900

    feat: ex3

--- src/Homework6.hs
+++ src/Homework6.hs
@@ -1,4 +1,4 @@
-module Homework6 (fibs1, fibs2) where
+module Homework6 (fibs1, fibs2, streamToList, Stream (Cons)) where
 
 fib :: Integer -> Integer
 fib 0 = 0
@@ -10,3 +10,11 @@ fibs1 = map fib [0 ..]
 
 fibs2 :: [Integer]
 fibs2 = 0 : 1 : zipWith (+) fibs2 (drop 1 fibs2)
+
+data Stream a = Cons a (Stream a)
+
+streamToList :: Stream a -> [a]
+streamToList (Cons x s) = x : streamToList s
+
+instance (Show a) => Show (Stream a) where
+  show s = show $ take 20 $ streamToList s
