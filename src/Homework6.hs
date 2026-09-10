5308260c8a0a40a08d886ae4976db813ad0f7676
Khanh Duong <dqkqdlot@gmail.com>
2026-09-11 07:39:17 +0900

    refactor: ex6 simplify and rename variables

--- src/Homework6.hs
+++ src/Homework6.hs
@@ -62,16 +62,5 @@ x = Cons 0 $ Cons 1 $ streamRepeat 0
 instance Num (Stream Integer) where
   fromInteger n = Cons n $ streamRepeat 0
   negate = streamMap (* (-1))
-  (+) (Cons l ls) (Cons r rs) = Cons (l + r) (ls + rs)
-
-  -- [a0, a1, a2, a3, a4, ...]
-  -- [b0, b1, b2, b3, b4, ...]
-  --
-  -- [b0a0, b0a1, b0a2, b0a3, b0a4, ...]
-  -- [   0, b1a0, b1a1, b1a2, b1a3, ...]
-  -- [   0,    0, b2a0, b2a1, b2a2, ...]
-  -- [   0,    0,    0, b3a0, b3a1, ...]
-  (*) (Cons l rest) rhs = mulStream l rhs + Cons 0 (rest * rhs)
-   where
-    mulStream :: Integer -> Stream Integer -> Stream Integer
-    mulStream m (Cons y ys) = Cons (m * y) $ mulStream m ys
+  (+) (Cons a0 a') (Cons b0 b') = Cons (a0 + b0) (a' + b')
+  (*) (Cons a0 a') b = streamMap (* a0) b + Cons 0 (a' * b)
