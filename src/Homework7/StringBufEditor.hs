8f842ba77b22cf0c1a895d9fc030727637371886
Khanh Duong <dqkqdlot@gmail.com>
2026-09-12 09:23:08 +0900

    feat: init homework7

---
+++ src/Homework7/StringBufEditor.hs
@@ -0,0 +1,13 @@
+{- FOURMOLU_DISABLE -}
+{-# OPTIONS_GHC -Wno-unused-imports -Wno-missing-signatures #-}
+module Homework7.StringBufEditor where
+
+import Homework7.StringBuffer
+import Homework7.Editor
+
+main = runEditor editor $ unlines
+         [ "This buffer is for notes you don't want to save, and for"
+         , "evaluation of steam valve coefficients."
+         , "To load a different file, type the character L followed"
+         , "by the name of the file."
+         ]
