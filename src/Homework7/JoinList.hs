a153bb82ca8740e95728ee4a1786c6031f871045
Khanh Duong <dqkqdlot@gmail.com>
2026-09-12 11:50:17 +0900

    feat: ex2 - indexJ

--- src/Homework7/JoinList.hs
+++ src/Homework7/JoinList.hs
@@ -1,4 +1,6 @@
-module Homework7.JoinList (JoinList (Empty, Single, Append), tag, (+++)) where
+module Homework7.JoinList (JoinList (Empty, Single, Append), tag, (+++), indexJ) where
+
+import Homework7.Sized (Sized (size), getSize)
 
 data JoinList m a
   = Empty
@@ -13,3 +15,21 @@ tag (Append m _ _) = m
 
 (+++) :: (Monoid m) => JoinList m a -> JoinList m a -> JoinList m a
 (+++) x y = Append (tag x <> tag y) x y
+
+actualSize :: (Sized b) => b -> Int
+actualSize = getSize . size
+
+indexJ :: (Sized b, Monoid b) => Int -> JoinList b a -> Maybe a
+-- the empty list have no element
+indexJ _ Empty = Nothing
+indexJ n (Single sz x)
+  -- the single list only contains one element
+  | n == 0 && actualSize sz == 1 = Just x
+  | otherwise = Nothing
+indexJ n x@(Append _ lhs rhs)
+  -- out of range
+  | n >= actualSize (tag x) = Nothing
+  -- only need to take the left
+  | n < actualSize (tag lhs) = indexJ n lhs
+  -- skip the left, take the index from the right
+  | otherwise = indexJ (n - actualSize (tag rhs)) rhs
--- test/TestHomework7.hs
+++ test/TestHomework7.hs
@@ -1,8 +1,11 @@
 module TestHomework7 (spec) where
 
-import Homework7.JoinList (JoinList (Append, Empty, Single), tag, (+++))
+import Homework7.JoinList (JoinList (Append, Empty, Single), indexJ, tag, (+++))
+import Homework7.Sized (Size)
 
+import Control.Monad (forM_)
 import Test.Hspec
+import Text.Printf (printf)
 
 spec :: Spec
 spec = do
@@ -20,3 +23,26 @@ spec = do
       it "(+++)" $
         Single "20" "30" +++ Single "40" "50"
           `shouldBe` Append "2040" (Single "20" "30") (Single "40" "50")
+
+    describe "Exercise 2" $ do
+      let singleJ = Single (1 :: Size)
+      let append = (singleJ "1" +++ singleJ "2") +++ (singleJ "3" +++ singleJ "4")
+      let cases =
+            [ (0 :: Int, Empty, Nothing)
+            , (1 :: Int, Empty, Nothing)
+            , -- single
+              (0 :: Int, Single (1 :: Size) "10", Just "10")
+            , (1 :: Int, Single 1 "10", Nothing)
+            , (2 :: Int, Single 1 "10", Nothing)
+            , (0 :: Int, Single 2 "10", Nothing)
+            , -- append
+              (0 :: Int, append, Just "1")
+            , (1 :: Int, append, Just "2")
+            , (2 :: Int, append, Just "3")
+            , (3 :: Int, append, Just "4")
+            , (4 :: Int, append, Nothing)
+            , (5 :: Int, append, Nothing)
+            ]
+      forM_ cases $ \(i, tree, expected) ->
+        it (printf "%d %s = %s" i (show tree) (show expected)) $
+          indexJ i tree `shouldBe` expected
